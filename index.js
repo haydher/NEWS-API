57400b6bd99bb9258443bca70a7b94acc03627b4
Haydher <haider0612@gmail.com>
2021-11-12 16:59:55 -0500

    cleaned up the code

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const news = require("./routes/news");
 const NewsAPI = require("newsapi");
 const newsApi = new NewsAPI(process.env.API_KEY);
-require("dotenv").config();
 
 app.use(cors());
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.get("/", (req, res) => {
  res.send("News Api");
@@ -42,14 +37,8 @@ app.get("/api/news", (req, res) => {
    res.send(response);
   });
 });
-// app.use("/api/news", news);
-
-// All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`);
+ console.log(`Example app listening at port: ${port}`);
 });
